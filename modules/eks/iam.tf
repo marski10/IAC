14c3f8f64cbb81e3264a712cf30e75e30e8c961a
marski10 <marquinhus.bill@hotmail.com>
2024-11-26 01:55:43 +0000

    teste

--- modules/eks/iam.tf
+++ modules/eks/iam.tf
@@ -200,11 +200,12 @@ resource "aws_iam_role_policy" "eks_elb"{
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["pods.eks.amazonaws.com"]
+   condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.main.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:alb-ingress-controller"]
     }
+
     principals {
       type        = "Federated"
       identifiers = [aws_iam_openid_connect_provider.main.arn]
@@ -234,4 +235,38 @@ resource "kubernetes_service_account" "main" {
   }
 
   depends_on = [local_file.kubeconfig]
+}
+
+
+data "aws_iam_policy_document" "aws_load_balancer_controller_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.main.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:alb-ingress-controller"]
+    }
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.main.arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "aws_load_balancer_controller" {
+  assume_role_policy = data.aws_iam_policy_document.aws_load_balancer_controller_assume_role_policy.json
+  name               = "aws-load-balancer-controller"
+}
+
+resource "aws_iam_policy" "aws_load_balancer_controller" {
+  policy = file("/workspaces/go-3/IAC/K8S/applications/iam-policy.json")
+  name   = "AWSLoadBalancerController"
+}
+
+resource "aws_iam_role_policy_attachment" "aws_load_balancer_controller_attach" {
+  role       = aws_iam_role.aws_load_balancer_controller.name
+  policy_arn = aws_iam_policy.aws_load_balancer_controller.arn
 }
